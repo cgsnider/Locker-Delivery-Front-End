586c27c94fa40114662627f2fc625c22a5b78d3e
Pranav Kandarpa <pranavkandarpa@gmail.com>
2022-12-04 23:34:14 -0500

    working with current user loc and hardcoded loc

--- Locker-Delivery-Front-End/MapView.swift
+++ Locker-Delivery-Front-End/MapView.swift
@@ -59,7 +59,6 @@ struct GeoView: UIViewRepresentable {
     
     @Binding var directions: [String]
     
-    
     func makeCoordinator() -> MapViewCoordinator {
         return MapViewCoordinator()
     }
@@ -107,6 +106,61 @@ struct GeoView: UIViewRepresentable {
     }
     
     class MapViewCoordinator: NSObject, MKMapViewDelegate {
+        
+        var locationManager: CLLocationManager?
+        
+        @Published var area = MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude:33.7833, longitude:-84.3831),
+            span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        )
+        
+        func ifEnabled() {
+            if CLLocationManager.locationServicesEnabled() {
+                locationManager = CLLocationManager()
+                locationManager?.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+    //            locationManager!.delegate = self
+            } else {
+                print("Location retrieval failed")
+            }
+        }
+
+        private func checkAuth() {
+            guard let locationManager = locationManager else { return }
+
+            switch locationManager.authorizationStatus {
+
+            case .notDetermined:
+                locationManager.requestWhenInUseAuthorization()
+            case .restricted:
+                print("Location not permitted")
+            case .denied:
+                print("Location service denied")
+            case .authorizedAlways:
+                area = MKCoordinateRegion(
+                    center: locationManager.location!.coordinate,
+                    span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+                    )
+            case .authorizedWhenInUse:
+                area = MKCoordinateRegion(
+                    center: locationManager.location!.coordinate,
+                    span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+                    )
+            @unknown default:
+                break
+            }
+        }
+
+        func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+            checkAuth()
+        }
+
+        func getLocation(for mapView: MKMapView) -> CLLocation {
+            let latitude = mapView.centerCoordinate.latitude
+            let longitude = mapView.centerCoordinate.longitude
+
+            return CLLocation(latitude: latitude, longitude: longitude)
+        }
+        
         func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
             let renderer = MKPolylineRenderer(overlay: overlay)
             renderer.strokeColor = .blue
